f705c87464cdad2453fa285595382d8c51d6d0f3
uppaljs <uppal@linux.net.pk>
2015-04-04 09:37:10 +0500

    Removed route.sh file

--- route.sh
+++
@@ -1,9 +0,0 @@
-/sbin/route add 199.48.164.101 gw 127.0.0.1
-/sbin/route add 195.154.58.164 gw 127.0.0.1
-/sbin/route add 195.154.39.5 gw 127.0.0.1
-/sbin/route add 195.154.48.200 gw 127.0.0.1
-/sbin/route add 69.197.155.26 gw 127.0.0.1
-/sbin/route add 62.210.251.48 gw 127.0.0.1
-/sbin/route add 62.210.248.235	gw 127.0.0.1
-/sbin/route add 104.245.101.166 gw 127.0.0.1
-
