579ce91655612cfa9c20e86a949431aafdfb46b7
uppaljs <uppal@linux.net.pk>
2015-04-04 08:57:22 +0500

    Added more IPs

--- route.sh
+++ route.sh
@@ -3,4 +3,7 @@
 /sbin/route add 195.154.39.5 gw 127.0.0.1
 /sbin/route add 195.154.48.200 gw 127.0.0.1
 /sbin/route add 69.197.155.26 gw 127.0.0.1
+/sbin/route add 62.210.251.48 gw 127.0.0.1
+/sbin/route add 62.210.248.235	gw 127.0.0.1
+/sbin/route add 104.245.101.166 gw 127.0.0.1
 
